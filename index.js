e620097fda8f0d43f9a85fbba92a7a638636ccbf
med9983-pixel <med447413@gmail.com>
2026-04-13 01:19:29 +0300

    update server

--- index.js
+++ index.js
@@ -5,29 +5,43 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+// صفحة رئيسية (مهم)
+app.get("/", (req, res) => {
+  res.send("🚀 Crash Game Server is Running");
+});
 
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: { origin: "*" }
 });
 
+// 🔥 العداد العالمي
 let globalCounter = 0;
 
 io.on("connection", (socket) => {
-  console.log("User connected");
+  console.log("User connected:", socket.id);
 
+  // إرسال القيمة الحالية
   socket.emit("counter", globalCounter);
 
+  // زيادة العداد
   socket.on("increment", () => {
     globalCounter++;
     io.emit("counter", globalCounter);
+    console.log("Counter:", globalCounter);
   });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected");
+    console.log("User disconnected:", socket.id);
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+// ❗ مهم لـ Railway
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
